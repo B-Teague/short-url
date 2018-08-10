680148afc89b020e78066791c39da210c8da9db4
Glitch (teague-apis-short-url) <none>
2018-08-10 02:18:12 +0000

    🍦🎩 Updated with Glitch

--- datastore.js
+++ datastore.js
@@ -2,7 +2,7 @@
 
 var mongodb = require('mongodb');
 // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
-var MONGODB_URI = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+'.mlab.com:'+process.env.DB_PORT+'/'+process.env.DB;
+var MONGODB_URI = 'mongodb://'+process.env.USER+':'+ encodeURIComponent(process.env.PASS) +'@'+process.env.HOST+'.mlab.com:'+process.env.DB_PORT+'/'+process.env.DB;
 var collection;
 
 // ------------------------------
